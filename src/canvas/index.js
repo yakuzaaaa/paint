7bfdd7723be9cb019a892df1d6f67b4cfc12f49d
nilarnab mookherjee <niluriju@gmail.com>
2019-09-23 07:54:31 +0530

    moved to non-passive touch events

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -3,7 +3,9 @@ const DRAW_TOOL = 'draw';
 const ERASE_TOOL = 'erase';
 
 const Canvas = ({ height, width, tool = DRAW_TOOL }) => {
-  let ctx = null;
+  let ctx = null,
+    mouseClicked = false;
+
   const canvasEl = useRef(null),
     _drawLine = (position) => {
       const x = position.clientX, y = position.clientY;
@@ -15,56 +17,71 @@ const Canvas = ({ height, width, tool = DRAW_TOOL }) => {
       ctx.clearRect(x - 25, y - 25, 50, 50);
     },
     _handleTouchMove = (e) => {
-      // e.preventDefault();
+      e.preventDefault();
       _handleMouseMove(e);
     },
+    _handleTouchStart = (e) => {
+      e.preventDefault();
+      _handleMouseDown(e);
+    },
     _handleMouseMove = (e) => {
-      let location = {
-        clientX: e.clientX,
-        clientY: e.clientY
-      };
+      if (mouseClicked) {
+        let location = {
+          clientX: e.clientX,
+          clientY: e.clientY
+        };
 
-      if (e.touches && !location.clientX) {
-        location.clientX = e.touches[0].clientX;
-        location.clientY = e.touches[0].clientY;
-      }
+        if (e.touches && !location.clientX) {
+          location.clientX = e.touches[0].clientX;
+          location.clientY = e.touches[0].clientY;
+        }
 
-      switch (tool) {
-        case ERASE_TOOL:
-          _erase(location);
-          break;
-        case DRAW_TOOL:
-        default:
-          _drawLine(location);
-          break;
+        switch (tool) {
+          case ERASE_TOOL:
+            _erase(location);
+            break;
+          case DRAW_TOOL:
+          default:
+            _drawLine(location);
+            break;
+        }
       }
     },
     _handleMouseUp = () => {
-      canvasEl.current.removeEventListener('mousemove', _handleMouseMove);
-      canvasEl.current.removeEventListener('touchmove', _handleTouchMove, { passive: false });
+      mouseClicked = false;
     },
     _handleMouseDown = () => {
       ctx.beginPath();
-      canvasEl.current.addEventListener('mousemove', _handleMouseMove);
-      canvasEl.current.addEventListener('touchmove', _handleTouchMove, { passive: false });
-      canvasEl.current.addEventListener('mouseup', _handleMouseUp);
+      mouseClicked = true;
     };
 
   useEffect(() => {
     canvasEl.current.height = height;
     canvasEl.current.width = width;
 
+
+    // React does not have any API to pass event listener options
+    // In this case, passive false
+    // Chrome by default has touch events passive by default
+    // We cannot prevent default on passive event. Thus this hack.
+    canvasEl.current.addEventListener('touchstart', _handleTouchStart, { passive: false });
+    canvasEl.current.addEventListener('touchmove', _handleTouchMove, { passive: false });
+
     ctx = canvasEl.current.getContext('2d');
+
+
+    return () => {
+      canvasEl.current.removeEventListener('touchstart', _handleTouchStart, { passive: false });
+      canvasEl.current.removeEventListener('touchmove', _handleTouchMove, { passive: false });
+    }
   }, [height, width]);
 
   return (
     <canvas
       ref={canvasEl}
       onMouseDown={_handleMouseDown}
-      onTouchStart={(event) => {
-        // event.preventDefault();
-        _handleMouseDown(event);
-      }}
+      onMouseUp={_handleMouseUp}
+      onMouseMove={_handleMouseMove}
     />
   );
 };
