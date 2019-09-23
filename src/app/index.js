271a0d993a633782c719d9324ff6a5920accddd1
nilarnab mookherjee <niluriju@gmail.com>
2019-09-23 07:29:54 +0530

    Removed react-hot-loader

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { hot } from "react-hot-loader";
 import "./app.css";
 import Canvas from '../canvas';
 
@@ -28,4 +27,4 @@ const App = () => {
   );
 };
 
-export default hot(module)(App);
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,12 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "public/"),
-    publicPath: "/public/",
-    filename: "paint.js"
+    filename: "paint.js",
+    path: path.resolve(__dirname, "public/")
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/public/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+    contentBase: "public",
+		port:3000,
+		open: true
+  }
 };
